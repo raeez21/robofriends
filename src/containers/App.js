cf49875fdc0035cffb78b5bddb30874ee4c3cf92
raeez21 <mohammadraeez.mec@gmail.com>
2024-02-20 16:06:31 +0530

    Ready for deploy

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll'
+import './App.css';
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state= {
+            robots:[],
+            searchfield: ''
+        }
+    }
+    // to make 'this' work the custom functions shoud be in arrow style
+    onSearchChange = (event) => {
+        this.setState({searchfield:event.target.value})
+        
+    }
+    componentDidMount(){
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response=>response.json())
+            .then(users=>this.setState({robots:users}))
+    }
+    render(){
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        
+        return !robots.length ? //if robots lenght is 0
+        <h1>Loading!!!</h1>: //return this as placeholder
+        ( //else proceed normally
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots = { filteredRobots }/>
+                    </Scroll>
+                </div>
+        )
+        
+        
+    }
+}
+
+export default App;
